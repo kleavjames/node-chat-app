fbb21e1cef84f1f8781b92f35618ffbd094a2f12
kleavjames <kleavantjames@gmail.com>
2018-05-21 00:44:27 +0800

    added form and functionality

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,24 @@ socket.on('connect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('new message: ', message);
+
+  const li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
 });
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function() {
+
+  })
+})
